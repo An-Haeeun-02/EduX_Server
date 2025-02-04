b02c3db224f38b798acc6d8d3f0829e7de582f52
hae <godma531@naver.com>
2025-02-04 18:07:49 +0900

    spring

---
+++ src/main/java/com/Capstone/EduX/EduXApplication.java
@@ -0,0 +1,13 @@
+package com.Capstone.EduX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Capstone/EduX/EduXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Capstone.EduX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
